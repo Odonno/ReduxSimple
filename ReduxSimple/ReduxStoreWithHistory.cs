fb951b2c373c6c266e54ac5d37db2f8de0533e26
mhusainisurge <37336747+mhusainisurge@users.noreply.github.com>
2018-03-30 07:25:40 -0500

    docs: add XML documentation #16
    * Adding XML documentation for exposed members of the library.
    
    * Fixing some typos.
    
    * Making the Reduce method protected.
    
    * Adding XML documentation for new methods.

--- ReduxSimple/ReduxStoreWithHistory.cs
+++ ReduxSimple/ReduxStoreWithHistory.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReduxSimple
 {
+    /// <summary>
+    /// The base class for creating predictable state containers that retain history and allow undo/redo operations.
+    /// </summary>
+    /// <typeparam name="TState">The type of the state.</typeparam>
+    /// <seealso cref="ReduxSimple.ReduxStore{TState}" />
     public abstract class ReduxStoreWithHistory<TState> : ReduxStore<TState>
         where TState : class, new()
     {
@@ -26,13 +29,28 @@ namespace ReduxSimple
         private readonly Subject<object> _undoneActionSubject = new Subject<object>();
         private readonly Stack<ReduxStoreMemento> _pastMementos = new Stack<ReduxStoreMemento>();
         private readonly Stack<object> _futureActions = new Stack<object>();
-        
+
+        /// <summary>
+        /// Gets a value indicating whether the undo operation can be performed.
+        /// </summary>
         public bool CanUndo => _pastMementos.Count != 0;
+        /// <summary>
+        /// Gets a value indicating whether the redo operation can be performed.
+        /// </summary>
         public bool CanRedo => _futureActions.Count != 0;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReduxStoreWithHistory{TState}"/> class.
+        /// </summary>
+        /// <param name="initialState">The initial state to put the store in; if <c>null</c>, a default value is constructed using <c>new TState()</c>.</param>
         protected ReduxStoreWithHistory(TState initialState = null) : base(initialState)
         { }
 
+        /// <summary>
+        /// Dispatches the specified action to the store, which reduces the current state of the store to a new state by performing the specified action
+        /// on the current state.
+        /// </summary>
+        /// <param name="action">The action to be performed on the current state.</param>
         public override void Dispatch(object action)
         {
             Dispatch(action, true);
@@ -49,6 +67,10 @@ namespace ReduxSimple
             base.Dispatch(action);
         }
 
+        /// <summary>
+        /// Undoes the last action that was performed, if any.
+        /// </summary>
+        /// <returns><c>true</c> if an action was undone; <c>false</c> otherwise.</returns>
         public bool Undo()
         {
             if (!CanUndo)
@@ -63,6 +85,10 @@ namespace ReduxSimple
             return true;
         }
 
+        /// <summary>
+        /// Redoes an operation, if any, that was previously undone.
+        /// </summary>
+        /// <returns><c>true</c> if an action was redone; <c>false</c> otherwise.</returns>
         public bool Redo()
         {
             if (!CanRedo)
@@ -75,15 +101,29 @@ namespace ReduxSimple
             return true;
         }
 
+        /// <summary>
+        /// Observes actions that are reversed (undone).
+        /// </summary>
+        /// <returns>An <see cref="IObservable{T}"/> that can be subscribed to in order to receive updates about actions that are reversed (undone).</returns>
         public IObservable<object> ObserveUndoneAction()
         {
             return _undoneActionSubject.AsObservable();
         }
+        /// <summary>
+        /// Observes actions of a specific type that are reversed (undone).
+        /// </summary>
+        /// <typeparam name="T">The type of actions that the subscriber is interested in.</typeparam>
+        /// <returns>
+        /// An <see cref="IObservable{T}"/> that can be subscribed to in order to receive updates whenever an action of <typeparamref name="T"/> is reversed (undone).
+        /// </returns>
         public IObservable<T> ObserveUndoneAction<T>() where T : class
         {
             return _undoneActionSubject.OfType<T>().AsObservable();
         }
 
+        /// <summary>
+        /// Resets the store to its initial state.
+        /// </summary>
         public override void Reset()
         {
             _pastMementos.Clear();
