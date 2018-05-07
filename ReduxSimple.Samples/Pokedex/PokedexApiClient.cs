b7b6bf51f286f0b6e9b9506b7f219207f48c4f14
Odonno <bottiau.david@laposte.net>
2018-05-07 12:57:48 +0200

    refactor(samples): create GetAsync method when calling web api

--- ReduxSimple.Samples/Pokedex/PokedexApiClient.cs
+++ ReduxSimple.Samples/Pokedex/PokedexApiClient.cs
@@ -4,6 +4,7 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace ReduxSimple.Samples.Pokedex
 {
@@ -21,32 +22,37 @@ namespace ReduxSimple.Samples.Pokedex
             }
         }
 
+        private async Task<T> GetAsync<T>(string url)
+        {
+            using (var httpClient = HttpClient)
+            {
+                var response = await HttpClient.GetAsync(url);
+
+                response.EnsureSuccessStatusCode();
+
+                var data = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+        }
+
         public IObservable<PokedexResponse> GetPokedex()
         {
             return Observable.Create<PokedexResponse>(async observer =>
-                {
-                    var response = await HttpClient.GetAsync("https://pokeapi.co/api/v1/pokedex");
-
-                    response.EnsureSuccessStatusCode();
-
-                    var data = await response.Content.ReadAsStringAsync();
-                    observer.OnNext(JsonConvert.DeserializeObject<PokedexResponse>(data));
-                })
-                .WithCache(() => _cacheService.Get<PokedexResponse>("pokedex"), r => _cacheService.Set("pokedex", r));
+            {
+                var result = await GetAsync<PokedexResponse>("https://pokeapi.co/api/v1/pokedex");
+                observer.OnNext(result);
+            })
+            .WithCache(() => _cacheService.Get<PokedexResponse>("pokedex"), r => _cacheService.Set("pokedex", r));
         }
 
         public IObservable<PokemonResponse> GetPokemonById(int id)
         {
             return Observable.Create<PokemonResponse>(async observer =>
-                {
-                    var response = await HttpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon/{id}");
-
-                    response.EnsureSuccessStatusCode();
-
-                    var data = await response.Content.ReadAsStringAsync();
-                    observer.OnNext(JsonConvert.DeserializeObject<PokemonResponse>(data));
-                })
-                .WithCache(() => _cacheService.Get<PokemonResponse>($"pokemon/{id}"), r => _cacheService.Set($"pokemon/{id}", r));
+            {
+                var result = await GetAsync<PokemonResponse>($"https://pokeapi.co/api/v2/pokemon/{id}");
+                observer.OnNext(result);
+            })
+            .WithCache(() => _cacheService.Get<PokemonResponse>($"pokemon/{id}"), r => _cacheService.Set($"pokemon/{id}", r));
         }
     }
 }
