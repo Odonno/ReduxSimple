d21211c1c28219d36c0315b31ddc70bb16131980
Odonno <bottiau.david@laposte.net>
2018-05-20 13:39:01 +0200

    feat: use cache to store state properties on full state comparer

--- ReduxSimple/FullStateComparer.cs
+++ ReduxSimple/FullStateComparer.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace ReduxSimple
 {
     internal class FullStateComparer<TState> : IEqualityComparer<TState>
         where TState : class, new()
     {
+        private readonly Dictionary<string, IEnumerable<PropertyInfo>> propertiesPerType = new Dictionary<string, IEnumerable<PropertyInfo>>();
+
         public bool Equals(TState x, TState y)
         {
             var stateType = typeof(TState);
 
-            var properties = stateType
-                .GetProperties()
-                .Where(p => p.CanRead && p.CanWrite);
+            var properties = GetStatePropertiesFromCache(stateType);
 
             foreach (var property in properties)
             {
@@ -22,13 +24,31 @@ namespace ReduxSimple
                 if (leftValue != rightValue)
                     return false;
             }
-            
+
             return true;
         }
 
         public int GetHashCode(TState obj)
         {
             return obj.GetHashCode();
+        }
+
+        private IEnumerable<PropertyInfo> GetStatePropertiesFromCache(Type stateType)
+        {
+            if (propertiesPerType.ContainsKey(stateType.FullName))
+            {
+                return propertiesPerType[stateType.FullName];
+            }
+            else
+            {
+                var properties = stateType
+                    .GetProperties()
+                    .Where(p => p.CanRead && p.CanWrite);
+
+                propertiesPerType.Add(stateType.FullName, properties);
+
+                return properties;
+            }
         }
     }
 }
