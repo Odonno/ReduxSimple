9029783b663e54d7db5e290a7e68b17444192e12
Odonno <bottiau.david@laposte.net>
2019-09-27 20:09:30 +0200

    tests: use updated CreateSelector function

--- ReduxSimple.UnitTests/Setup/TodoListStore/Selectors.cs
+++ ReduxSimple.UnitTests/Setup/TodoListStore/Selectors.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using static ReduxSimple.Selectors;
@@ -8,11 +7,17 @@ namespace ReduxSimple.UnitTests.Setup.TodoListStore
 {
     public static class Selectors
     {
-        public static Func<TodoListState, ImmutableList<TodoItem>> SelectTodoList = state => state.TodoList;
-        public static Func<TodoListState, string> SelectCurrentUser = state => state.CurrentUser;
-        public static Func<TodoListState, string> SelectUselessProperty = state => state.UselessProperty;
+        public static ISelectorWithoutProps<TodoListState, ImmutableList<TodoItem>> SelectTodoList = CreateSelector(
+            (TodoListState state) => state.TodoList
+        );
+        public static ISelectorWithoutProps<TodoListState, string> SelectCurrentUser = CreateSelector(
+            (TodoListState state) => state.CurrentUser
+        );
+        public static ISelectorWithoutProps<TodoListState, string> SelectUselessProperty = CreateSelector(
+            (TodoListState state) => state.UselessProperty
+        );
 
-        public static MemoizedSelectorWithProps<TodoListState, string, ImmutableList<TodoItem>, IEnumerable<TodoItem>> SelectSearchedItems = CreateSelector(
+        public static ISelectorWithProps<TodoListState, string, IEnumerable<TodoItem>> SelectSearchedItems = CreateSelector(
             SelectTodoList,
             (ImmutableList<TodoItem> items, string search) =>
             {
